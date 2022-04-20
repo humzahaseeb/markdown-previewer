59b059f3b33d35da5ebb84aa0221e4bc012d9c8a
Humza Haseeb <hamzahaseeb5@gmail.com>
2022-04-20 20:08:10 +0500

    markdown functionality added

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,13 +1,58 @@
 import React, {useState} from 'react'
-import Preview from './Preview'
+import ReactMarkdown from 'react-markdown'
+
 
 const Editor = () => {
-  const [markdown, setMarkdown] = useState("")
-  const [preview, setPreview] = useState('')
+
+  const placeholder = `# Welcome to my React Markdown Previewer!
+
+  ## This is a sub-heading...
+  ### And here's some other cool stuff:
+
+  Heres some code, \`<div></div>\`, between 2 backticks.
+
+  \`\`\`
+  // this is multi-line code:
+
+  function anotherExample(firstLine, lastLine) {
+    if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+      return multiLineCode;
+    }
+  }
+  \`\`\`
+
+  You can also make text **bold**... whoa!
+  Or _italic_.
+  Or... wait for it... **_both!_**
+  And feel free to go crazy ~~crossing stuff out~~.
+
+  There's also [links](https://www.freecodecamp.org), and
+  > Block Quotes!
+
+  And if you want to get really crazy, even tables:
+
+  Wild Header | Crazy Header | Another Header?
+  ------------ | ------------- | -------------
+  Your content can | be here, and it | can be here....
+  And here. | Okay. | I think we get it.
+
+  - And of course there are lists.
+    - Some are bulleted.
+      - With different indentation levels.
+          - That look like this.
+
+
+  1. And there are numbered lists too.
+  1. Use just 1s if you want!
+  1. And last but not least, let's not forget embedded images:
+
+  ![React Logo](data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K)
+  `;
+
+  const [markdown, setMarkdown] = useState(placeholder)
 
   const handleChange = (e) => {
     setMarkdown(e.target.value)
-    setPreview(e.target.value)
   }
 
   return (
@@ -18,7 +63,13 @@ const Editor = () => {
               {markdown}
             </textarea>
         </div>
-        <Preview preview={preview}/>
+
+        <div className='preview-div'>
+          <h2>Previewer</h2>
+          <div id='preview'>
+            <ReactMarkdown>{markdown}</ReactMarkdown>
+          </div>
+        </div>
     </div>
   )
 }
